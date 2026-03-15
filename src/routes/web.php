dec44ea15394d507863a9a37bddb36f4a616ffd8
saaaaku <saaaaku@gmail.com>
2026-03-15 22:05:20 +0900

    修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,12 +17,13 @@ use App\Http\Controllers\CategoryController;
 
 Route::middleware('auth')->group(function(){
   Route::get('/',[TodoController::class,'index']);
+  Route::post('/todos',[TodoController::class, 'store']);
+  Route::patch('/todos/update',[TodoController::class, 'update']);
+  Route::delete('/todos/delete',[TodoController::class, 'destroy']);
+  Route::get('/todos/search',[TodoController::class,'search']);
+  Route::get('/categories',[CategoryController::class, 'index']);
+  Route::post('/categories',[CategoryController::class,'store']);
+  Route::patch('/categories/update',[CategoryController::class,'update']);
+  Route::delete('/categories/delete',[CategoryController::class,'destroy']);
 });
-Route::post('/todos',[TodoController::class, 'store']);
-Route::patch('/todos/update',[TodoController::class, 'update']);
-Route::delete('/todos/delete',[TodoController::class, 'destroy']);
-Route::get('/todos/search',[TodoController::class,'search']);
-Route::get('/categories',[CategoryController::class, 'index']);
-Route::post('/categories',[CategoryController::class,'store']);
-Route::patch('/categories/update',[CategoryController::class,'update']);
-Route::delete('/categories/delete',[CategoryController::class,'destroy']);
+
